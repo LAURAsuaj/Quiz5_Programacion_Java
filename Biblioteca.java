d8949d3bf1d93cb6cb22a934d3f0280267a00761
Laurasuaj <laurita@gmail.com>
2023-10-03 15:18:55 -0500

    Se crearon las tres clases del quiz

---
+++ Biblioteca.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+
+public class Biblioteca {
+    private LinkedList<Libro> librosDisponibles = new LinkedList<>();
+
+    public void registrarLibro(Libro [] Libro) {
+
+        void titulo() {System.out.println("Nombre del libro" + librosDisponibles );
+    }
+    public Libro buscarLibro(String [] titulo) {
+
+        return null;
+    }
+
+    public LinkedList<Libro> mostrarLibrosDisponibles() {
+        return librosDisponibles;
+    }
+}
+
